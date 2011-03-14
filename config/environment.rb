3d0a2c0f56cf6e81827632bf4cdd926ac70b1066
Adam Soltys <adam.soltys@pwgsc-tpsgc.gc.ca>
2011-03-14 15:56:13 -0600

    get utf8 working with mysql adapter

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,7 @@
 # -*- encoding : utf-8 -*-
 # Load the rails application
 require File.expand_path('../application', __FILE__)
+require 'mysql_utf8'
 
 # Initialize the rails application
 Masquerade::Application.initialize!
