40f03d338cb89d8f4816016188352a1241323ff8
Adam Soltys <adam.soltys@pwgsc-tpsgc.gc.ca>
2011-03-14 15:08:27 -0600

    remove exception notification

--- config/initializers/exception_notification.rb
+++
@@ -1,6 +0,0 @@
-if ExceptionNotifier
-  Masquerade::Application.config.middleware.use ExceptionNotifier,
-    :email_prefix => "[#{Masquerade::Application::Config['name']} Error] ",
-    :sender_address => %("#{Masquerade::Application::Config['name']}" <#{Masquerade::Application::Config['mailer']['from']}>),
-    :exception_recipients => [Masquerade::Application::Config['email']]
-end
